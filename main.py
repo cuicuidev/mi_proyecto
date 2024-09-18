64e1cbff4bcea6cb3d790a3bde550cbf3b2d5442
Dmitry Ryzhenkov <cuicui@arch>
2024-09-18 21:27:52 +0200

    añadimos dataframe del iris

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import seaborn as sns
 
 def main():
     st.title("Hola Mundo")
     st.write("Hola Mamá")
+
+    df = sns.load_dataset("iris")
+    st.dataframe(df)
 
 if __name__ == "__main__":
     main()
